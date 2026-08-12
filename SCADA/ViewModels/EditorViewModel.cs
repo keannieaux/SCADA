c384a51c906c74b73ac90f24b23e190651320745
keannieaux <keannieaux@gmail.com>
2026-08-12 17:15:48 +0300

    добавила редактор шапку

--- SCADA/ViewModels/EditorViewModel.cs
+++ SCADA/ViewModels/EditorViewModel.cs
@@ -4,5 +4,7 @@ namespace SCADA.ViewModels;
 
 public partial class EditorViewModel : ViewModelBase
 {
+    public string[] Modes {get; } = ["Разработка", "Исполненение"];
 
+    [ObservableProperty] public partial string SelectedMode {get; set; }="Разработка";
 }
